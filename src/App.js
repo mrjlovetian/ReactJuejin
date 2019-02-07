9df1d5cc84dbed20d1988a85a341fdaff44bf8b3
MRJ <mrjlovetian@gmail.com>
2019-02-08 00:48:56 +0800

    add file

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React, { Component } from 'react';
+import MianView from './mainapp/mainView'
 
 class App extends Component {
-
-  componentDidMount = () => {
-    this.getData()
-  }
-
-  getData=()=>{
-    console.log('begin request!!!')
-    fetch('http://192.168.152.130:3000/getjuejin?pagesize=1',{'method':'GET'})
-    .then((response)=>{
-      if (response.status === 200) {
-        console.log('................', response);
-        response.json().then((data)=>{
-          console.log('aaaaaaaaaaaaaaaaaaaaaaa', data.data)
-        })
-      }
-     
-    })
-    .catch((err)=>{
-      console.log('//////////////////////', err);
-      
-    })
-  }
   
   render() {
     return (
       <div className="App">
-       
+      <MianView />
       </div>
     );
   }
---
+++ src/mainapp/mainView.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+
+export default class mainView extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      datas:[]
+    }
+  }
+
+  componentDidMount(){
+    this.getData()
+  }
+
+  getData(){
+    console.log('开始请求数据')
+    fetch('http://192.168.1.107:3000/juejin?pagesize=200',{"method":"GET"})
+    .then((response)=>{
+      if(response.status===200){
+        response.json().then((datas)=>{
+          console.log('.............', datas)
+          var jueDatas = datas["data"]
+          this.setState({
+            datas:jueDatas
+          })
+        })
+      }
+    })
+    .catch((err)=>{
+
+    })
+  }
+
+  getdate(obj) {
+    var second = parseFloat(obj)
+    var date =  new Date(second);
+    var y = 1900+date.getYear();
+    var m = "0"+(date.getMonth()+1);
+    var d = "0"+date.getDate();
+    return y+"-"+m.substring(m.length-2,m.length)+"-"+d.substring(d.length-2,d.length);
+  }
+
+  renderView(){
+    return this.state.datas.map((item)=>{
+      return (<div style={itemview} key={item.content}>
+        <div style={top}>
+          <div style={title}>{item.title}</div>
+          {item.screenshot.length>0?<div style={img}><img  style={img} src={item.screenshot} alt='hh'/></div>:null}
+        </div>
+        <div style={content}>{item.content}</div>
+        <div style={bottom}>
+          <div style={time}>{this.getdate(item.buildTime)}</div>
+          <div style={info}>{item.summaryInfo}</div>
+        </div>
+      </div>)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderView()}
+      </div>
+    )
+  }
+}
+
+const itemview = {
+  margin:'10px'
+}
+
+const top = {
+  display:'flex',
+  justifyContent:'space-between',
+}
+
+const title = {
+  width:'100px',
+  textOverflow:'ellipsis',
+  overflow:'hidden',
+  whiteSpace:'nowrap'
+}
+
+const img = {
+  height:"50px",
+  width:'50px'
+}
+
+const content = {
+  textOverflow:'ellipsis',
+  overflow:'hidden',
+  display:'-webkit-box',
+  WebkitBoxOrient:'vertical',
+  WebkitLineClamp:'3'
+}
+
+const bottom = {
+  marginTop:'5px',
+  display:"flex",
+  justifyContent:'spacebetween'
+}
+
+const time = {
+  height:'20px',
+}
+const info = {
+  height:'20px',
+  textOverflow:'ellipsis',
+  overflow:'hidden',
+  whiteSpace:'nowrap',
+  width:'100px'
+}
